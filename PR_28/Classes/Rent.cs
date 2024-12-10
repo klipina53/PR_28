c2885413c0ce3b188f4155d9f4eb6087a640f43f
klipina53 <klipina53@gmail.com>
2024-12-10 20:06:56 +0500

    Созданный класс Rent.Липина

---
+++ PR_28/Classes/Rent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR_28.Classes
+{
+    public class Rent
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public DateTime dateandtime { get; set; }
+        public string fio { get; set; }
+        public Rent(int id, string name, DateTime dateandtime, string fio)
+        {
+            this.id = id;
+            this.name = name;
+            this.dateandtime = dateandtime;
+            this.fio = fio;
+        }
+    }
+}
